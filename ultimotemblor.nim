af7025d0db8b2b4a3320dd8dd9cec9056f2e93e5
Alejandro <49617228+fariascl@users.noreply.github.com>
2021-09-12 11:45:29 -0300

    update API url
    URL de la API actualizada

--- ultimotemblor.nim
+++ ultimotemblor.nim
@@ -5,7 +5,7 @@ type
     Fecha, Profundidad, Magnitud, RefGeografica : string
 
 var client = newHttpClient()
-var x = client.getContent("https://api.gael.cl/general/public/sismos")
+var x = client.getContent("https://api.gael.cloud/general/public/sismos")
 let jsonObject = parseJson(x)
 let temblor = to(jsonObject[0], Element)
 echo "Programita para saber dónde fue el último temblor\nFecha: ", temblor.Fecha, "\nReferencia geográfica: ", temblor.RefGeografica, "\nLa magnitud fue de: ", temblor.Magnitud, "\nProfundidad: ", temblor.Profundidad, " Km" 
